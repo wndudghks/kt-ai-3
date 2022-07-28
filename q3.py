e9172b51c8ad54f30d07233fb281c3ae25ae2151
joo <joo@a.com>
2022-07-28 16:37:27 +0900

    fix1

--- q3.py
+++ q3.py
@@ -2,7 +2,7 @@ def solution(left, right):
     answer = 0
     for num in range(left, right+1):
         count=0
-        for i in range(2,num):#1과 자기 자신, 2개의 약수를 count하지 않음
+        for i in range(1,num+1):
             if num%i==0:
                 count+=1
         if count%2==0:
